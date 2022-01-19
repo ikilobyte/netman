fa05f9f2dab44d2688a85fe1e63664f3edd7491a
laravelup <laraveler@163.com>
2022-01-19 11:04:26 +0800

    UnPack返回参数

--- iface/ipacker.go
+++ iface/ipacker.go
@@ -3,5 +3,5 @@ package iface
 //IPacker 数据封装抽象层
 type IPacker interface {
 	Pack(msgID uint32, data []byte) ([]byte, error) // 封包
-	UnPack([]byte) IMessage                         // 解包
+	UnPack([]byte) (IMessage, error)                // 解包
 }

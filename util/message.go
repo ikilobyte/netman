08702b2469cde087af15adac589535fe27534045
laravelup <laraveler@163.com>
2022-01-19 10:42:19 +0800

    message的抽象

--- util/message.go
+++ util/message.go
@@ -1,13 +1,28 @@
 package util
 
-type message struct {
-	dataBuff []byte
+//Message 收到数据的封装
+type Message struct {
+	MsgID   uint32 // 消息ID
+	DataLen int    // 消息长度
+	Data    []byte // 消息
 }
 
-func NewMessage(dataBuff []byte) *message {
-	return &message{dataBuff: dataBuff}
+func NewMessage(data []byte) *Message {
+	return &Message{Data: data, DataLen: len(data)}
 }
 
-func (m *message) Bytes() []byte {
-	return m.dataBuff
+func (m *Message) GetMsgID() uint32 {
+	return m.MsgID
+}
+
+func (m *Message) String() string {
+	return string(m.Data)
+}
+
+func (m *Message) Bytes() []byte {
+	return m.Data
+}
+
+func (m *Message) Len() int {
+	return m.DataLen
 }

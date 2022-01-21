f6abf44de248db09bb5d5c6760d0c5c99a640a59
laravelup <laraveler@163.com>
2022-01-21 09:22:25 +0800

    默认实现

--- server/router.go
+++ server/router.go
@@ -1,15 +1,10 @@
 package server
 
 import (
-	"fmt"
-
 	"github.com/ikilobyte/netman/iface"
 )
 
 type BaseRouter struct{}
 
-func (b *BaseRouter) Do(request iface.IRequest) {
-	conn := request.GetConnect()
-	message := request.GetMessage()
-	fmt.Println(conn, message)
-}
+//Do 默认实现
+func (b *BaseRouter) Do(request iface.IRequest) {}

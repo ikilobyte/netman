c75782878b07dcaa3b7eec690d9a3e3464384674
sunny <laraveler@163.com>
2022-02-27 15:23:19 +0800

    epoll.go 兼容TLS

--- eventloop/epoll.go
+++ eventloop/epoll.go
@@ -3,10 +3,7 @@
 package eventloop
 
 import (
-	"crypto/tls"
-	"fmt"
 	"io"
-	"net"
 
 	"github.com/ikilobyte/netman/common"
 	"github.com/ikilobyte/netman/util"
@@ -61,7 +58,6 @@ func (p *Poller) Wait(emitCh chan<- iface.IRequest) {
 				conn   iface.IConnect
 			)
 
-			fmt.Println("event.events", event.Events)
 			// 1、通过connID获取conn实例
 			if conn = p.ConnectMgr.Get(connFd); conn == nil {
 				// 断开连接
@@ -84,22 +80,15 @@ func (p *Poller) Wait(emitCh chan<- iface.IRequest) {
 				continue
 			}
 
-			// 1、判断是否开启tls，需要完成tls握手
+			// 1、判断是否开启tls
 			if conn.GetTLSEnable() && conn.GetHandshakeCompleted() == false {
 
-				fmt.Println("开始握手！！！？？")
-
-				//tls.ReadHeaderPayload(conn)
-				//
-				//time.Sleep(time.Hour)
-				tlsConn := tls.Server(conn.(net.Conn), &tls.Config{Certificates: []tls.Certificate{conn.GetCertificate()}})
-				// tls握手失败
-				if err := tlsConn.Handshake(); err != nil {
+				tlsConnect := conn.GetTLSConnect()
+				if err := tlsConnect.Handshake(); err != nil {
 					p.ClearByConn(conn)
 					util.Logger.Errorf("tls handshake error %v", err)
 					continue
 				}
-
 				// 1、设置状态
 				conn.SetHandshakeCompleted()
 
@@ -108,14 +97,10 @@ func (p *Poller) Wait(emitCh chan<- iface.IRequest) {
 					p.ClearByConn(conn)
 					continue
 				}
-
-				fmt.Println("tls握手成功！")
-				continue
 			}
 
-			fmt.Println("???????!!!???")
 			// 2、读取一个完整的包
-			message, err := conn.GetPacker().ReadFull(connFd)
+			message, err := conn.GetPacker().ReadFull(conn)
 			if err != nil {
 				switch err {
 				case io.EOF, util.HeadBytesLengthFail, util.BodyLenExceedLimit:

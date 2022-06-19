f6b3cd562a56532e1fce80abdf15ec3112cf00a8
sunny <laraveler@163.com>
2022-06-19 12:40:41 +0800

    middleware example

--- examples/server/main.go
+++ examples/server/main.go
@@ -50,6 +50,62 @@ func (h *HelloRouter) Do(request iface.IRequest) {
 	// connect.Close()
 }
 
+type UserInfoRoute struct {
+}
+
+func (u *UserInfoRoute) Do(request iface.IRequest) {
+	fmt.Println("Through middleware")
+	fmt.Println(request.GetMessage().Bytes())
+}
+
+//global 全局中间件
+func global() iface.MiddlewareFunc {
+	return func(ctx iface.IContext, next iface.Next) interface{} {
+
+		fmt.Println("Front middleware")
+		fmt.Println("ctx data", ctx.GetConnect(), ctx.GetRequest(), ctx.GetMessage())
+
+		ctx.Set("key", "value")
+		ctx.Set("now", time.Now().UnixNano())
+
+		// 继续往下执行
+		resp := next(ctx)
+
+		fmt.Println("Rear middleware")
+		return resp
+	}
+}
+
+func demo() iface.MiddlewareFunc {
+	return func(ctx iface.IContext, next iface.Next) interface{} {
+		fmt.Println("demo middleware start")
+		fmt.Printf("key=%v now=%v\n", ctx.Get("key"), ctx.Get("now"))
+		resp := next(ctx)
+		fmt.Println("demo middleware end")
+		return resp
+	}
+}
+
+var loginStore map[int]time.Time
+
+//authentication 分组中间件
+func authentication() iface.MiddlewareFunc {
+
+	return func(ctx iface.IContext, next iface.Next) interface{} {
+
+		conn := ctx.GetConnect()
+		// 是否登录过
+		if _, ok := loginStore[conn.GetID()]; !ok {
+			_, _ = conn.Send(1, []byte("Authentication failed"))
+			_ = conn.Close()
+			return nil
+		}
+
+		return next(ctx)
+
+	}
+}
+
 func main() {
 
 	fmt.Println(os.Getpid())
@@ -70,10 +126,23 @@ func main() {
 		server.WithHeartbeatIdleTime(time.Second*180),     // 表示一个连接如果180秒内未向服务器发送任何数据，此连接将被强制关闭
 	)
 
+	// 全局中间件，每个路由都会执行
+	s.Use(global())
+	s.Use(demo())
+
 	// 根据业务需求，添加路由
 	s.AddRouter(0, new(HelloRouter))
 	//s.AddRouter(1, new(XXRouter))
 	// ...
+
+	// 分组中间件
+	g := s.Group(authentication())
+	{
+		g.AddRouter(1, new(UserInfoRoute))
+		//g.AddRouter(2,new(xxx))
+		//g.AddRouter(3,new(xxx))
+		//g.AddRouter(4,new(xxx))
+	}
 
 	// 启动
 	s.Start()

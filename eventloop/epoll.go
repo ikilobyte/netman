d5d2a02de9aff0a7c9fad363cf15a6ff5d4bd56f
Sunny <ikilobyte@163.com>
2022-09-16 10:55:52 +0800

    兼容websocket空payload的消息

--- eventloop/epoll.go
+++ eventloop/epoll.go
@@ -113,8 +113,8 @@ func (p *Poller) Wait(emitCh chan<- iface.IContext) {
 				}
 			}
 
-			// 3、将消息传递出去，交给worker处理
-			if message == nil || message.Len() <= 0 {
+			// 3、将消息传递出去，交给worker处理（websocket是可以发送payload长度为0的消息）
+			if message == nil || (message.Len() <= 0 && message.IsWebsocket() == false) {
 				continue
 			}
 
